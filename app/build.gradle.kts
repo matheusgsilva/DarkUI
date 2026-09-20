926cf00aceb391d82ad947eb56de6532bc034ba7
matheusgsilva <49157527+matheusgsilva@users.noreply.github.com>
2026-09-20 20:26:19 -0300

    fix: migrate Kotlin compiler options

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
     buildFeatures { compose = true }
     packaging {
         resources.excludes += setOf("/META-INF/{AL2.0,LGPL2.1}", "META-INF/DEPENDENCIES")
